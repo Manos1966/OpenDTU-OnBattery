9c68e9851b670732158f7bc70a30b17bded3be36
Manos1966 <129660360+Manos1966@users.noreply.github.com>
2026-02-24 21:10:57 +0100

    Update Stats.cpp
    changes to the MQTT publish

--- src/battery/pylontech/Stats.cpp
+++ src/battery/pylontech/Stats.cpp
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include <MqttSettings.h>
 #include <battery/pylontech/Stats.h>
-#include "MqttHandleInverter.h"
+#include <MqttHandleInverter.h>
 
 namespace Batteries::Pylontech {
 
